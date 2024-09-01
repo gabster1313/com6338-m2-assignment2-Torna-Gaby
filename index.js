5b26f5c393c11e21c45e20152b6cf6a9b7239a8c
Gaby <gabytorna@gmail.com>
2024-09-01 19:22:26 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function greet(){
+    let person = prompt ("what is your name?");
+    alert ('Hello,' + person );
+
+    let age = prompt ('How old are you?');
+        console.log(parseInt(age));
+
+    let hadbirthday = confirm ('Have you had a birthday this year?');
+    console.log(hadbirthday)
+
+    let year = new Date().getFullYear();
+
+    let birthyear;
+    if (hadbirthday){
+        console.log(birthyear = year - age);
+    } else {
+        console.log(birthyear = year - age - 1);
+    }
+
+    alert('You were born in' +  birthyear);
+        
+    }
